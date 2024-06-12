3a4456937b38b4225bb811830de1ad66d63d5bab
caomingyu <1054527636@qq.com>
2024-06-12 16:24:01 +0800

    tmp

--- moe_prune/eval_ppl.sh
+++ moe_prune/eval_ppl.sh
@@ -4,20 +4,20 @@
 # python eval_ppl.py --score-mode test_route
 
 # prune by l1, one layer
-python eval_ppl.py --score-mode l1 --layer-mode one_layer
+python moe_prune/eval_ppl.py --score-mode l1 --layer-mode one_layer
 
 # prune by weightwatcher alpha, one layer
-python eval_ppl.py --score-mode ww_alpha --layer-mode one_layer
+python moe_prune/eval_ppl.py --score-mode ww_alpha --layer-mode one_layer
 
 # prune by random, one layer
-python eval_ppl.py --score-mode random --layer-mode one_layer
+python moe_prune/eval_ppl.py --score-mode random --layer-mode one_layer
 
 # prune by l1
-python eval_ppl.py --score-mode l1 --layer-mode jump_layers
+python moe_prune/eval_ppl.py --score-mode l1 --layer-mode jump_layers
 
 # prune by weightwatcher alpha
-python eval_ppl.py --score-mode ww_alpha --layer-mode jump_layers
+python moe_prune/eval_ppl.py --score-mode ww_alpha --layer-mode jump_layers
 
 # prune by random
-python eval_ppl.py --score-mode random --layer-mode jump_layers
+python moe_prune/eval_ppl.py --score-mode random --layer-mode jump_layers
 
