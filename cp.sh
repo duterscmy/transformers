ed32348e6288885e9b5b23ccb0858bf3c5597f34
caomingyu <1054527636@qq.com>
2024-06-20 20:31:29 +0800

    fix

---
+++ cp.sh
@@ -0,0 +1,1 @@
+cp deepseek_model/modeling_deepseek.py /root/autodl-tmp/deepseek-ai/deepseek-moe-16b-base/
--- moe_prune/eval_ppl.sh
+++ moe_prune/eval_ppl.sh
@@ -5,7 +5,7 @@
 model_path=/root/autodl-tmp/deepseek-ai/deepseek-moe-16b-base/
 # model_path=/root/autodl-tmp/qw27/
 # prune by l1, one layer
-python moe_prune/eval_ppl.py --score-mode l1 --layer-mode one_layer --model ${model_path}
+python moe_prune/eval_ppl.py --score-mode l1 --layer-mode one_layer --model ${model_path} --num-layer 27
 
 # prune by weightwatcher alpha, one layer
 # python moe_prune/eval_ppl.py --score-mode ww_alpha --layer-mode one_layer --model ${model_path}
@@ -20,5 +20,5 @@ python moe_prune/eval_ppl.py --score-mode l1 --layer-mode one_layer --model ${mo
 # python moe_prune/eval_ppl.py --score-mode ww_alpha --layer-mode jump_layers --model ${model_path}
 
 # prune by random
-python moe_prune/eval_ppl.py --score-mode random --layer-mode jump_layers --model ${model_path}
+python moe_prune/eval_ppl.py --score-mode random --layer-mode jump_layers --model ${model_path} --num-layer 27
 
