80637ecc04aaff2d57659cfb034a045709b3ad06
caomingyu <1054527636@qq.com>
2024-07-02 19:48:54 +0800

    fix

--- deepseek_model/modeling_deepseek_infer.py
+++ deepseek_model/modeling_deepseek_infer.py
@@ -454,7 +454,7 @@ class DeepseekMoE(nn.Module):
                     inputs, prune_expert_idxs, relative_layer)
             else:
                 print("layer_num {} current_layer {}, use ROUTE layer".format(
-                    self.layer_num, self.global_layer))
+                    self.layer_num, global_layer))
                 output = self.forward_route(inputs)
         except Exception as e:
             err_msg = traceback.format_exc()
