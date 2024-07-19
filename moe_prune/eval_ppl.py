fix bug and renew kl function

moe_prune/eval_ppl.py
@@ -25,6 +25,9 @@ def calculate_kl_divergence(probs_p, probs_q):
     :param probs_q: 近似分布的概率分布 (形状：[N, D])
     :return: KL散度
     """
+    epsilon = 1e-10
+    probs_p = probs_p + epsilon
+    probs_q = probs_q + epsilon
     kl_div = F.kl_div(probs_q.log(), probs_p, reduction='batchmean')  # 计算KL散度
     return kl_div
 
@@ -40,22 +43,22 @@ def calculate_kl_divergence(p, q):
     q = q + epsilon
     return (p * (p.log() - q.log())).mean()
 
-def calculate_js_divergence(logits_p, logits_q):
-    """
-    计算两个分布之间的Jensen-Shannon散度
-    :param logits_p: 真实分布的logits (形状：[N, D])
-    :param logits_q: 近似分布的logits (形状：[N, D])
-    :return: JS散度
-    """
-    p = F.softmax(logits_p, dim=-1)  # 将logits转化为概率分布
-    q = F.softmax(logits_q, dim=-1)  # 将logits转化为概率分布
+# def calculate_js_divergence(logits_p, logits_q):
+#     """
+#     计算两个分布之间的Jensen-Shannon散度
+#     :param logits_p: 真实分布的logits (形状：[N, D])
+#     :param logits_q: 近似分布的logits (形状：[N, D])
+#     :return: JS散度
+#     """
+#     p = F.softmax(logits_p, dim=-1)  # 将logits转化为概率分布
+#     q = F.softmax(logits_q, dim=-1)  # 将logits转化为概率分布
 
-    m = 0.5 * (p + q)
-    kl_pm = calculate_kl_divergence(p, m)
-    kl_qm = calculate_kl_divergence(q, m)
-    js_div = 0.5 * (kl_pm + kl_qm)
-    print("kl per sample {} {} {}".format(kl_pm, kl_qm, js_div))
-    return js_div
+#     m = 0.5 * (p + q)
+#     kl_pm = calculate_kl_divergence(p, m)
+#     kl_qm = calculate_kl_divergence(q, m)
+#     js_div = 0.5 * (kl_pm + kl_qm)
+#     print("kl per sample {} {} {}".format(kl_pm, kl_qm, js_div))
+#     return js_div
 
 
 def get_layer_output(model, moe_layer_idx, tokenizer, input_strs, add_special_tokens=True, ):
@@ -257,13 +260,13 @@ try:
             output_dict["expert_idxs"].append(tmp_prune_expert_idx_list)
             output_dict["expert_num"].append(len(tmp_prune_expert_idx_list))
 
-            if mean_jl < optimal_ppl:
-                optimal_ppl = mean_jl
+            if mean_jl < optimal_jl:
+                optimal_jl = mean_jl
                 optimal_candidate_idx = candidate_idx
 
             end_time = time.time()
             print("jl {}, best_jl {}, eval jl cost {} seconds".format(
-                mean_jl, optimal_ppl, end_time-start_time))
+                mean_jl, optimal_jl, end_time-start_time))
 
         prune_expert_idx_list = prune_expert_idx_list + [optimal_candidate_idx]
 