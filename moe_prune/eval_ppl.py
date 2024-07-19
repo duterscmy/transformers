fix bug and renew kl function

moe_prune/eval_ppl.py
@@ -31,34 +31,34 @@ def calculate_kl_divergence(probs_p, probs_q):
     kl_div = F.kl_div(probs_q.log(), probs_p, reduction='batchmean')  # 计算KL散度
     return kl_div
 
-def calculate_kl_divergence(p, q):
-    """
-    计算两个分布之间的KL散度
-    :param p: 真实分布的概率 (形状：[N, D])
-    :param q: 近似分布的概率 (形状：[N, D])
-    :return: KL散度
-    """
-    epsilon = 1e-10
-    p = p + epsilon
-    q = q + epsilon
-    return (p * (p.log() - q.log())).mean()
-
-# def calculate_js_divergence(logits_p, logits_q):
+# def calculate_kl_divergence(p, q):
 #     """
-#     计算两个分布之间的Jensen-Shannon散度
-#     :param logits_p: 真实分布的logits (形状：[N, D])
-#     :param logits_q: 近似分布的logits (形状：[N, D])
-#     :return: JS散度
+#     计算两个分布之间的KL散度
+#     :param p: 真实分布的概率 (形状：[N, D])
+#     :param q: 近似分布的概率 (形状：[N, D])
+#     :return: KL散度
 #     """
-#     p = F.softmax(logits_p, dim=-1)  # 将logits转化为概率分布
-#     q = F.softmax(logits_q, dim=-1)  # 将logits转化为概率分布
+#     epsilon = 1e-10
+#     p = p + epsilon
+#     q = q + epsilon
+#     return (p * (p.log() - q.log())).mean()
 
-#     m = 0.5 * (p + q)
-#     kl_pm = calculate_kl_divergence(p, m)
-#     kl_qm = calculate_kl_divergence(q, m)
-#     js_div = 0.5 * (kl_pm + kl_qm)
-#     print("kl per sample {} {} {}".format(kl_pm, kl_qm, js_div))
-#     return js_div
+def calculate_js_divergence(logits_p, logits_q):
+    """
+    计算两个分布之间的Jensen-Shannon散度
+    :param logits_p: 真实分布的logits (形状：[N, D])
+    :param logits_q: 近似分布的logits (形状：[N, D])
+    :return: JS散度
+    """
+    p = F.softmax(logits_p, dim=-1)  # 将logits转化为概率分布
+    q = F.softmax(logits_q, dim=-1)  # 将logits转化为概率分布
+
+    m = 0.5 * (p + q)
+    kl_pm = calculate_kl_divergence(p, m)
+    kl_qm = calculate_kl_divergence(q, m)
+    js_div = 0.5 * (kl_pm + kl_qm)
+    print("kl per sample {} {} {}".format(kl_pm, kl_qm, js_div))
+    return js_div
 
 
 def get_layer_output(model, moe_layer_idx, tokenizer, input_strs, add_special_tokens=True, ):