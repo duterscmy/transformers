3870d2034504cf66d64b72ec2929ef03e9df485a
caomingyu <1054527636@qq.com>
2024-07-31 17:34:53 +0800

    mod:simplify the code

--- moe_prune/finetune_weight_all.py
+++ moe_prune/finetune_weight_all.py
@@ -18,10 +18,11 @@ from typing import List, Optional
 import random
 import os
 import json
-# import shortuuid
 import time
 
 from transformers.models.qwen2_moe.expert_idx import *
+from .config import get_layer_idx_order, dynamic_weights, get_layer_idx_to_expert_idx
+from .utils import print_trainable_parameters
 
 
 parser = argparse.ArgumentParser()
@@ -37,7 +38,6 @@ parser.add_argument("--num-layer", type=int, default=27,
                     help="默认为qw16B层数")  # deepseek 27 qw24
 parser.add_argument("--num-expert", type=int, default=64, help="默认为qw16B专家数")
 
-
 parser.add_argument("--score-mode", type=str, default="l1", help="层间对专家排序的指标")
 parser.add_argument("--prune-num-expert", default=6, type=int,
                     help="剪枝后剩余的expert数量")
@@ -49,7 +49,15 @@ parser.add_argument("--reverse-experts", action="store_true",
 args = parser.parse_args()
 
 pytorch_checkpoint_path = args.model
-# @param ["", "0", "0,1", "0,1,2"] {allow-input: true}
+batch_size = args.batch_size
+score_mode = args.score_mode
+num_layer = args.num_layer
+num_expert = args.num_expert
+prune_num_expert = args.prune_num_expert
+prune_num_layer = args.prune_num_layer
+output_dir = args.output_dir
+
+
 available_gpu_ids_str = "0"
 memory_per_gpu = "48GiB"  # @param ["", "38GiB"] {allow-input: true}
 cpu_memory = '80GiB'  # @param ["50GiB"] {allow-input: true}
@@ -96,81 +104,9 @@ model = AutoModelForCausalLM.from_pretrained(
 tokenizer = AutoTokenizer.from_pretrained(pytorch_checkpoint_path)
 
 
-batch_size = args.batch_size
-score_mode = args.score_mode
-num_layer = args.num_layer
-num_expert = args.num_expert
-prune_num_expert = args.prune_num_expert
-prune_num_layer = args.prune_num_layer
-
 # prune layer idx and expert idx
-if score_mode == "l1":
-    layer_idx_to_expert_idxs = json.load(
-        open("deepseek_model/layer_idx_to_expert_idx.json", 'r'))
-    layer_idx_to_expert_idxs = {
-        int(key): value for key, value in layer_idx_to_expert_idxs.items()}
-elif score_mode == "ww_alpha":
-    layer_idx_to_expert_idxs = json.load(
-        open("deepseek_model/layer_idx_to_expert_idx.alpha.json", 'r'))
-    layer_idx_to_expert_idxs = {
-        int(key): value for key, value in layer_idx_to_expert_idxs.items()}
-elif score_mode == "distribution":
-    layer_idx_to_expert_idxs = json.load(
-        open("deepseek_model/layer_idx_to_expert_idx.distribution.json", 'r'))
-    layer_idx_to_expert_idxs = {
-        int(key): value for key, value in layer_idx_to_expert_idxs.items()}
-elif score_mode == "random":
-    layer_idx_to_expert_idxs = json.load(
-        open("deepseek_model/layer_idx_to_expert_idx.random.json", 'r'))
-    layer_idx_to_expert_idxs = {
-        int(key): value for key, value in layer_idx_to_expert_idxs.items()}
-elif score_mode == "greedy_jl":
-    layer_idx_to_expert_idxs = json.load(
-        open("deepseek_model/layer_idx_to_expert_idx.greedy_jl.json", 'r'))
-    layer_idx_to_expert_idxs = {
-        int(key): value for key, value in layer_idx_to_expert_idxs.items()}
-
-
-# load dynamic weights
-dynamic_weight_tmp = json.load(open("deepseek_model/dynamic_weight.json"))
-for key, value in dynamic_weight_tmp.items():
-    key = key.split("-")
-    layer_idx = int(key[0])
-    expert_idx = int(key[1])
-    w = value[-1]
-    dynamic_weights[(layer_idx, expert_idx)] = w
-
-# ppl order pruning single layer
-if prune_num_expert == 0:
-    # layer_idx_list_ppl_order = [11, 18, 7, 8, 2, 23, 10, 22, 13, 16,
-    #                             15, 20, 24, 19, 25, 4, 6, 5, 3, 9, 21, 27, 17, 12, 26, 14, 1]
-    # layer_idx_list_ppl_order = [layer-1 for layer in layer_idx_list_ppl_order]
-    layer_idx_list_ppl_order = [10, 17, 22, 1, 12,
-                                21, 6, 15, 7, 19, 24, 9]
-    layer_idx_list_ppl_order = [19, 12, 7, 23, 10, 14,
-                                1, 24, 17, 15, 9, 21, 18, 6, 26]  # greedy by jl
-elif prune_num_expert == 6 and score_mode == "random":
-    # layer_idx_list_ppl_order = [11, 18, 7, 23, 15, 8, 10, 2, 22, 20,
-    #                             24, 16, 13, 6, 3, 19, 25, 4, 5, 9, 21, 27, 17, 12, 26, 14, 1]
-    # layer_idx_list_ppl_order = [layer-1 for layer in layer_idx_list_ppl_order]
-    layer_idx_list_ppl_order = [10, 17, 22, 1, 9,
-                                6, 21, 15, 12, 14, 19, 7]
-elif prune_num_expert == 6 and score_mode == "l1":
-    layer_idx_list_ppl_order = [5, 18, 11, 22, 8, 13, 10, 7, 23, 16,
-                                2, 20, 4, 24, 15, 19, 9, 3, 25, 6, 17, 1, 21, 27, 14, 12, 26]
-    layer_idx_list_ppl_order = [layer-1 for layer in layer_idx_list_ppl_order]
-    layer_idx_list_ppl_order = [4, 17, 21, 10, 22,
-                                12, 7, 15, 9, 19, 6, 18]
-elif prune_num_expert == 6 and score_mode == "distribution":
-    layer_idx_list_ppl_order = [15, 10, 7, 18, 8, 2, 22, 16, 23, 11,
-                                20, 24, 13, 6, 19, 25, 4, 3, 5, 1, 27, 9, 21, 17, 12, 26, 14]
-    layer_idx_list_ppl_order = [layer-1 for layer in layer_idx_list_ppl_order]
-    layer_idx_list_ppl_order = [14, 9, 1, 21, 22,
-                                6, 17, 10, 12, 7, 15, 24]
-elif prune_num_expert == 6 and score_mode == "greedy_jl":
-    layer_idx_list_ppl_order = [19, 15, 22, 10,
-                                12, 6, 14, 21, 26, 7, 17, 1, 24, 23, 9]
-
+layer_idx_to_expert_idxs = get_layer_idx_to_expert_idx(score_mode)
+layer_idx_list_ppl_order = get_layer_idx_order(prune_num_expert, score_mode)
 
 # add expert weight to prune layer
 for param in model.parameters():
@@ -181,14 +117,11 @@ prune_layer_idx_to_expert_idx = {}
 for prune_layer_idx in layer_idx_list_ppl_order[:prune_num_layer]:
     prune_expert_idx_list = layer_idx_to_expert_idxs[prune_layer_idx][:prune_num_expert]
     prune_layer_idx_to_expert_idx[prune_layer_idx] = prune_expert_idx_list
-# set global variable
-prune_layer_list.append(prune_layer_idx_to_expert_idx)
-layer_num_list.append(num_layer)
+print(f"prune layer to expert: {prune_layer_idx_to_expert_idx}")
 
 
-# add lora to model
 def check_if_lora(module_name):
-    # available extra experts
+    '''classify the remained experts in prune layers'''
     try:
         layer_id = int(module_name.split(".")[2])
         expert_id = int(module_name.split(".")[5])
@@ -202,7 +135,7 @@ def check_if_lora(module_name):
 
 
 def check_if_lora_2(module_name):
-    # shared experts
+    '''classify the shared experts in prune layers'''
     try:
         layer_id = int(module_name.split(".")[2])
     except:
@@ -214,29 +147,8 @@ def check_if_lora_2(module_name):
     return False
 
 
-for name, module in model.named_modules():
-    if isinstance(module, (torch.nn.Linear)) and (check_if_lora(name) or check_if_lora_2(name)):
-        for param in module.parameters():
-            param.requires_grad = True
-
-
-def print_trainable_parameters(model):
-    trainable_params = 0
-    all_param = 0
-    for _, param in model.named_parameters():
-        all_param += param.numel()
-        if param.requires_grad:
-            trainable_params += param.numel()
-    print(
-        f"trainable params: {trainable_params} || all params: {all_param} || trainable%: {100 * trainable_params / all_param:.2f}"
-    )
-
-
-print_trainable_parameters(model)
-
-
 def check_if_prune(module_name):
-    # prune experts
+    '''classify the pruned experts in prune layers'''
     try:
         layer_id = int(module_name.split(".")[2])
         expert_id = int(module_name.split(".")[5])
@@ -249,7 +161,14 @@ def check_if_prune(module_name):
     return False
 
 
-print(prune_layer_idx_to_expert_idx)
+# set //remained experts and shared experts// of prune layer to require gradient
+for name, module in model.named_modules():
+    if isinstance(module, (torch.nn.Linear)) and (check_if_lora(name) or check_if_lora_2(name)):
+        for param in module.parameters():
+            param.requires_grad = True
+print_trainable_parameters(model)
+
+# set //prune experts// of prune layer to empty to reduce memory
 num_prune_module = 0
 for name, module in model.named_modules():
     if isinstance(module, (torch.nn.Linear)) and check_if_prune(name):
@@ -259,7 +178,7 @@ for name, module in model.named_modules():
             param.requires_grad = False
             param.data = torch.tensor(
                 0.1, dtype=param.dtype, device=param.device)
-print("prune {} module".format(num_prune_module))
+print("set {} modules to empty".format(num_prune_module))
 print_trainable_parameters(model)
 
 # finetune
@@ -269,10 +188,7 @@ dataset = load_dataset('json', data_files=[
 eval_dataset = load_dataset(
     'json', data_files=["datasets/sample_questions_from_6_dataset.json"])
 
-
-# 假设你正在使用GPT-2模型（你可以根据需要更改为其他模型）
 tokenizer = AutoTokenizer.from_pretrained(pytorch_checkpoint_path)
-# 定义tokenize函数
 
 
 def tokenize_function(examples):
@@ -290,10 +206,8 @@ tokenized_datasets = dataset.map(
 eval_tokenized_datasets = eval_dataset.map(
     tokenize_function, batched=True, remove_columns=["text"])
 
-
 output_file = "finetune_all_score_mode_{}_layer_{}_expert{}".format(
     score_mode, prune_num_layer, prune_num_expert)
-output_dir = args.output_dir
 if not os.path.exists(output_dir):
     os.mkdir(output_dir)
 
