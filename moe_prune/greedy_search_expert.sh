b1397c70d68017c3760d65e48cf39297d352c3d6
caomingyu <1054527636@qq.com>
2024-08-01 12:09:27 +0800

    mod

--- moe_prune/greedy_search_expert.sh
+++ moe_prune/greedy_search_expert.sh
@@ -1,11 +1,14 @@
 #!/bin/bash  
 # 遍历0到63之间的每一个数字，代表要剪枝的层索引 
-output_path="greedy_search_expert_output_by_jl_c4"
+input=$1
+output_path=$2
+
 mkdir -p $output_path
 for layer_idx in $(seq 0 26); do  
     # 执行Python脚本，传递模型路径和要剪枝的层索引  
     python moe_prune/greedy_search_expert.py \
         --model /root/autodl-tmp/deepseek-ai/deepseek-moe-16b-base \
+        --input $input \
         --prune-layer $layer_idx  \
         --output $output_path \
         > $output_path/${layer_idx}.search.log
