24475693a6576a1a8a7d6953397cac9ff9535340
caomingyu <1054527636@qq.com>
2024-08-01 12:07:55 +0800

    mod

--- moe_prune/greedy_search_expert.sh
+++ moe_prune/greedy_search_expert.sh
@@ -1,8 +1,14 @@
 #!/bin/bash  
-# 遍历0到63之间的每一个数字，代表要剪枝的层索引  
-for layer_idx in $(seq 0 5); do  
+# 遍历0到63之间的每一个数字，代表要剪枝的层索引 
+output_path="greedy_search_expert_output_by_jl_c4"
+mkdir -p $output_path
+for layer_idx in $(seq 0 26); do  
     # 执行Python脚本，传递模型路径和要剪枝的层索引  
-    python moe_prune/greedy_search_expert.py --model /root/autodl-tmp/deepseek-ai/deepseek-moe-16b-base --prune-layer $layer_idx  > greedy_search_expert_output_by_jl/${layer_idx}.search.log
+    python moe_prune/greedy_search_expert.py \
+        --model /root/autodl-tmp/deepseek-ai/deepseek-moe-16b-base \
+        --prune-layer $layer_idx  \
+        --output $output_path \
+        > $output_path/${layer_idx}.search.log
     # 可选：如果需要查看每个循环的输出，可以在命令后添加echo来打印当前层索引  
     echo "Evaluated prune layer: $layer_idx"  
 done
