b7442284a7a4cb25cdf257e3f01733a5f082f507
caomingyu <1054527636@qq.com>
2024-08-01 11:45:05 +0800

    add

--- moe_prune/greedy_search_per_layer.sh
+++ moe_prune/greedy_search_per_layer.sh
@@ -2,7 +2,7 @@
 # 遍历0到63之间的每一个数字，代表要剪枝的层索引  
 for layer_idx in $(seq 0 26); do  
     # 执行Python脚本，传递模型路径和要剪枝的层索引  
-    python moe_prune/eval_ppl.py --model ./deepseek16b --prune-layer $layer_idx  
+    python moe_prune/greedy_search_expert.py --model ./deepseek16b --prune-layer $layer_idx  
     # 可选：如果需要查看每个循环的输出，可以在命令后添加echo来打印当前层索引  
     echo "Evaluated prune layer: $layer_idx"  
 done
