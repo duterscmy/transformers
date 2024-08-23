15e8213ddbce1b25e6e08b4224257a7776211405
caomingyu <1054527636@qq.com>
2024-08-23 21:34:37 +0800

    add:

---
+++ moe_prune/eval_ppl_simple.py
@@ -0,0 +1,114 @@
+# -*- coding: utf-8 -*-
+import pandas as pd
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import argparse
+import torch
+from accelerate import infer_auto_device_map, init_empty_weights, load_checkpoint_and_dispatch
+from transformers import AutoTokenizer, T5Tokenizer, AutoConfig, AutoModelForCausalLM, LogitsProcessorList, LogitsProcessor
+from typing import List, Optional
+import random
+import os
+import json
+# import shortuuid
+import time
+
+
+def compute_ppl(model, tokenizer, input_strs, gen_kwargs,
+                add_special_tokens=True, split_special_tokens=False, output_only=True, verbose=False):
+
+    model = model.eval()
+
+    # Tokenization
+    def encode_text_batch(input_strs):
+        inputs = tokenizer.batch_encode_plus(input_strs,
+                                             padding='longest',
+                                             #  add_special_tokens=add_special_tokens,
+                                             #  split_special_tokens=split_special_tokens,
+                                             return_tensors="pt")
+        input_ids = inputs.input_ids.to(model.device)
+        attention_mask = inputs.attention_mask.to(model.device)
+        return input_ids
+
+    batch_size = 1  # 批处理大小
+    num_texts = len(input_strs)
+    loss_sum = 0.0
+
+    for i in range(0, len(input_strs), batch_size):
+        text_list_batch = input_strs[i:i+batch_size]
+        input_ids = encode_text_batch(text_list_batch)
+        with torch.no_grad():
+            outputs = model(input_ids, labels=input_ids)
+            loss = outputs.loss.mean()
+        loss_sum += loss.item()
+
+    mean_loss = loss_sum / num_texts  # 计算整个数据集的损失均值
+    mean_ppl = torch.exp(torch.tensor(mean_loss))
+    return mean_ppl
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--input", default="./moe_prune/data/questions.jsonl",
+                    help="MTBench数据集路径")
+parser.add_argument("--model", default="./deepseek",
+                    help="模型路径")
+
+args = parser.parse_args()
+
+pytorch_checkpoint_path = args.model
+# @param ["", "0", "0,1", "0,1,2"] {allow-input: true}
+available_gpu_ids_str = "0"
+memory_per_gpu = "38GiB"  # @param ["", "38GiB"] {allow-input: true}
+cpu_memory = '50GiB'  # @param ["50GiB"] {allow-input: true}
+model_dtype = 'bfloat16'  # @param ["float32", "bfloat16"]
+offload = False  # @param {type:"boolean"}
+
+if torch.cuda.is_available():
+    cuda_list = available_gpu_ids_str.split(',')
+else:
+    available_gpu_ids_str, memory_per_gpu = "", ""
+    model_dtype = "bfloat16"
+    cuda_list = []
+
+no_split_module_classes = "OpenMoeDecoderLayer"
+
+# 1. Allocate Devices for Inference
+available_memory = {int(cuda): memory_per_gpu for cuda in cuda_list}
+available_memory['cpu'] = cpu_memory
+
+# 2. Load the Model (init with empty weight to save memory)
+config = AutoConfig.from_pretrained(
+    pytorch_checkpoint_path, trust_remote_code=True)
+# weights_location = snapshot_download(repo_id=pytorch_checkpoint_path)
+with init_empty_weights():
+    model = AutoModelForCausalLM.from_config(config,
+                                             torch_dtype=eval(
+                                                 f'torch.{model_dtype}'),
+                                             trust_remote_code=True)
+device_map = infer_auto_device_map(model,
+                                   max_memory=available_memory,
+                                   no_split_module_classes=no_split_module_classes)
+
+
+model = AutoModelForCausalLM.from_pretrained(
+    pytorch_checkpoint_path,
+    device_map=device_map,
+    torch_dtype=torch.bfloat16,
+    trust_remote_code=True,
+    # offload_folder="offload",
+    # offload_state_dict=True,
+    # dtype=eval(f'torch.{model_dtype}'),
+    # no_split_module_classes=[no_split_module_classes]
+)
+tokenizer = AutoTokenizer.from_pretrained(pytorch_checkpoint_path)
+# read benchmark
+with open(args.input, 'r') as fp:
+    questions = []
+    for line in fp:
+        line = line.strip()
+        if line:
+            question = json.loads(line)
+            questions.append(question)
+raw_questions = list(map(lambda x: x["turns"][0], questions))
+
+for _ in range(10):
+    mean_ppl = compute_ppl(model, tokenizer, raw_questions, None)
