d7c65aeb5e07915f9ac8ff09874fee37a3112e11
caomingyu <1054527636@qq.com>
2024-08-30 16:09:20 +0800

    fix

--- src/transformers/models/qwen2_moe/expert_idx.py
+++ src/transformers/models/qwen2_moe/expert_idx.py
@@ -1,9 +1,9 @@
 from multiprocessing import Manager
 
 manager = Manager()
-# expert_idxs_list  = manager.list()
-# global_layer_list = manager.list()
+expert_idxs_list  = manager.list()
+global_layer_list = manager.list()
 prune_layer_list = manager.list()
 layer_num_list = manager.list()
-# global_layer_list.append(0)
+global_layer_list.append(0)
 dynamic_weights = manager.dict()
