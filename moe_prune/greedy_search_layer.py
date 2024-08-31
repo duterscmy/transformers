f4297b087e7cc3fee263a331d09223785304e83a
caomingyu <1054527636@qq.com>
2024-08-31 10:14:03 +0800

    add: truncation for greedy layer

--- moe_prune/greedy_search_layer.py
+++ moe_prune/greedy_search_layer.py
@@ -112,6 +112,10 @@ parser.add_argument("--input", default="./datasets/sample_eval.json",
                     help="eval数据集路径")
 parser.add_argument("--model", default="./deepseek",
                     help="模型路径")
+parser.add_argument("--dynamic-weight-file", default="./",
+                    help="动态路由系数")
+parser.add_argument("--greedy-expert-file",  default="./",
+                    help="逐层贪心搜索的专家")
 parser.add_argument("--batch-size", type=int, default=8, help="并行解码的样本数量")
 parser.add_argument("--num-layer", type=int, default=27,
                     help="默认为qw16B层数")  # deepseek 27 qw24
@@ -127,8 +131,15 @@ parser.add_argument("--reverse-experts", action="store_true",
                     help="如果指定，则剪枝时倒转expert顺序")
 
 args = parser.parse_args()
-
+batch_size = args.batch_size
+num_layer = args.num_layer
+num_expert = args.num_expert
+num_prune_expert = args.prune_expert
+prune_expert_strategy = args.prune_expert_strategy
 pytorch_checkpoint_path = args.model
+dynamic_weight_file = args.dynamic_weight_file
+greedy_expert_file = args.greedy_expert_file
+
 # @param ["", "0", "0,1", "0,1,2"] {allow-input: true}
 available_gpu_ids_str = "0"
 memory_per_gpu = "38GiB"  # @param ["", "38GiB"] {allow-input: true}
@@ -171,10 +182,6 @@ model = AutoModelForCausalLM.from_pretrained(
     device_map=device_map,
     torch_dtype=torch.bfloat16,
     trust_remote_code=True,
-    # offload_folder="offload",
-    # offload_state_dict=True,
-    # dtype=eval(f'torch.{model_dtype}'),
-    # no_split_module_classes=[no_split_module_classes]
 )
 tokenizer = AutoTokenizer.from_pretrained(pytorch_checkpoint_path)
 
@@ -196,23 +203,25 @@ with open(args.input, 'r') as fp:
             questions.append(question)
 raw_questions = list(map(lambda x: x["text"], questions))
 
-
-batch_size = args.batch_size
-num_layer = args.num_layer
-num_expert = args.num_expert
-num_prune_expert = args.prune_expert
-prune_expert_strategy = args.prune_expert_strategy
-
 # load dynamic weights
 if num_prune_expert == 0:
     layer_idx_to_expert_idxs = {idx: [] for idx in range(27)}
-elif num_prune_expert >0 and prune_expert_strategy == "greedy_jl":
+elif prune_expert_strategy == "greedy_jl":
     layer_idx_to_expert_idxs = json.load(
         open("deepseek_model/layer_idx_to_expert_idx.greedy_jl.json", 'r'))
     layer_idx_to_expert_idxs = {
         int(key): value[:num_prune_expert] for key, value in layer_idx_to_expert_idxs.items()}
+elif prune_expert_strategy == "greedy_jl_c4":
+    layer_idx_to_expert_idxs = json.load(
+        open("deepseek_model/layer_idx_to_expert_idx.greedy_jl.c4.json", 'r'))
+    layer_idx_to_expert_idxs = {
+        int(key): value[:num_prune_expert] for key, value in layer_idx_to_expert_idxs.items()}
 
-dynamic_weight_tmp = json.load(open("deepseek_model/dynamic_weight.json"))
+if prune_expert_strategy == "greedy_jl":
+    dynamic_weight_file = "deepseek_model/dynamic_weight.json"
+elif prune_expert_strategy == "greedy_jl_c4":
+    dynamic_weight_file = "deepseek_model/dynamic_weight.c4.json"
+dynamic_weight_tmp = json.load(open(dynamic_weight_file, 'r'))
 for key, value in dynamic_weight_tmp.items():
     key = key.split("-")
     layer_idx = int(key[0])
@@ -294,7 +303,7 @@ try:
 
     output_df = pd.DataFrame(output_dict)
     output_df.to_excel(
-        "greedy_search_layer_prune_expert{}_stratedy{}_jl_beam{}.xlsx".format(num_prune_expert, prune_expert_strategy, beam_size))
+        "greedy_search_layer_prune_expert{}_stratedy{}_jl_beam{}.c4.xlsx".format(num_prune_expert, prune_expert_strategy, beam_size))
 except Exception as e:
     import traceback
     msg = traceback.format_exc()
